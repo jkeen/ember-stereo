c58e0fa93f8f6772021c1db3f9d93df6f43fb0a7
Jeff Keen <jeff@keen.me>
2021-08-23 19:48:08 -0500

    Clean up a couple of things, downgrade auto import for now

--- addon/-private/utils/evented.js
+++ addon/-private/utils/evented.js
@@ -10,28 +10,28 @@ import EmberEvented from '@ember/object/evented';
  *
  */
 export default class Evented {
-    eventManager = EmberObject.extend(EmberEvented).create();
+  eventManager = EmberObject.extend(EmberEvented).create();
 
-    on(name, target, method) {
-      this.eventManager.on(name, target, method);
-      return this;
-    }
+  on(name, target, method) {
+    this.eventManager.on(name, target, method);
+    return this;
+  }
 
-    off(name, target, method) {
-      this.eventManager.off(name, target, method);
-      return this;
-    }
+  off(name, target, method) {
+    this.eventManager.off(name, target, method);
+    return this;
+  }
 
-    one(name, target, method) {
-      this.eventManager.one(name, target, method);
-      return this;
-    }
+  one(name, target, method) {
+    this.eventManager.one(name, target, method);
+    return this;
+  }
 
-    has(name) {
-      return this.eventManager.has(name);
-    }
+  has(name) {
+    return this.eventManager.has(name);
+  }
 
-    trigger(name, ...args) {
-      this.eventManager.trigger(name, ...args);
-    }
+  trigger(name, ...args) {
+    this.eventManager.trigger(name, ...args);
+  }
 }
