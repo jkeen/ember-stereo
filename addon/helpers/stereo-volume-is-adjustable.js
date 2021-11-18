a41e759b0ff4580f6700ba02f96a65482b61110c
Jeff Keen <jeff@keen.me>
2021-11-18 00:09:31 -0600

    feat: add {{stereo-volume-is-adjustable}} helper, which returns whether or not the device supports adjusting volume at the sound level

---
+++ addon/helpers/stereo-volume-is-adjustable.js
@@ -0,0 +1,29 @@
+import { inject as service } from '@ember/service';
+import Helper from '@ember/component/helper';
+
+
+/**
+  returns true if not mobile device. Mobile devices can't have audio adjusted at the stereo/sound level.
+
+  ```hbs
+    {{#if (stereo-volume-is-adjustable)}}
+      //show volume controls here
+    {{/if}}
+
+    ```
+  @class {{stereo-volume}}
+  @type {Helper}
+  */
+
+/**
+  @method compute
+  @return {Integer}
+*/
+export default class StereoVolumeIsAdjustable extends Helper {
+  @service stereo;
+
+  compute() {
+    return !this.stereo.isMobileDevice;
+  }
+}
+
---
+++ app/helpers/stereo-volume-is-adjustable.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-stereo/helpers/stereo-volume-is-adjustable';
