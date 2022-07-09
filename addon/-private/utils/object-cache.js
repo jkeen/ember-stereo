36c224469549418f3c56abd419c99bbcaef65ef4
Mark Oleson <fusion2004@gmail.com>
2022-07-09 10:43:43 -0400

    always check if the identifier is an object before attempting to use WeakMap APIs

--- addon/-private/utils/object-cache.js
+++ addon/-private/utils/object-cache.js
@@ -1,45 +1,46 @@
 import { tracked } from '@glimmer/tracking';
 import normalizeIdentifier from './normalize-identifier';
-import { inject as service } from '@ember/service';
-import {
-  TrackedObject,
-  TrackedWeakMap,
-} from 'tracked-built-ins';
+import { TrackedObject, TrackedWeakMap } from 'tracked-built-ins';
+
+function isAnObject(identifier) {
+  return identifier.then || typeof identifier === 'object';
+}
 
 /**
-* This class caches things based on a strings or objects. You shouldn't have to interact with this class.
-*/
-
+ * This class caches things based on a strings or objects. You shouldn't have to interact with this class.
+ */
 export default class ObjectCache {
-  @service stereo;
-
   @tracked objectCache = new TrackedWeakMap();
-  @tracked keyCache = new TrackedObject()
-  name = 'ember-stereo:object-cache'
+  @tracked keyCache = new TrackedObject();
+  name = 'ember-stereo:object-cache';
 
   has(_identifier) {
     let identifier = normalizeIdentifier(_identifier);
-    return this.objectCache.has(identifier) || (identifier in this.keyCache)
+    if (isAnObject(identifier)) {
+      return this.objectCache.has(identifier);
+    } else {
+      return identifier in this.keyCache;
+    }
   }
 
   find(_identifier) {
     let identifier = normalizeIdentifier(_identifier);
 
-    if (this.objectCache.has(identifier)) {
-      return this.objectCache.get(identifier)
+    if (isAnObject(identifier) && this.objectCache.has(identifier)) {
+      return this.objectCache.get(identifier);
     } else if (this.keyCache[identifier]) {
-      return this.keyCache[identifier]
+      return this.keyCache[identifier];
     }
   }
 
   remove(_identifier) {
     let identifier = normalizeIdentifier(_identifier);
 
-    if (this.objectCache.has(identifier)) {
-      this.objectCache.remove(identifier)
+    if (isAnObject(identifier) && this.objectCache.has(identifier)) {
+      this.objectCache.delete(identifier);
     }
     if (this.keyCache[identifier]) {
-      delete this.keyCache[identifier]
+      delete this.keyCache[identifier];
     }
   }
 
@@ -47,10 +48,10 @@ export default class ObjectCache {
     let identifier = normalizeIdentifier(_identifier);
 
     if (identifier) {
-      if (identifier.then || (typeof identifier === 'object')) {
-        this.objectCache.set(identifier, value)
+      if (isAnObject(identifier)) {
+        this.objectCache.set(identifier, value);
       } else {
-        this.keyCache[identifier] = value
+        this.keyCache[identifier] = value;
       }
     }
   }
