2a2d3974ab784a20b8363a2dda673ab454c97087
Mark Oleson <fusion2004@gmail.com>
2022-07-11 17:46:26 -0400

    fix: use object spread operator to replace Ember.assign and fix deprecation

--- addon/-private/utils/prepare-options.js
+++ addon/-private/utils/prepare-options.js
@@ -1,9 +1,6 @@
-import { assign } from '@ember/polyfills';
-
-
 // Prepares options for template helpers
 export default function prepareOptions(options) {
-  let newOptions = assign({silenceErrors: true}, options);
+  let newOptions = { silenceErrors: true, ...options };
   // newOptions.remote = remote;
   // if (!newOptions.metadata) {
   //   newOptions.metadata = metadata;
