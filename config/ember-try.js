871c7e534aee7a4dbfd46fb1cda3c7192909cd41
Jeff Keen <jeff@keen.me>
2022-12-12 23:29:37 -0600

    chore!: update to Ember 4.8, modify release settings
    BREAKING CHANGE: drop support for Ember < 3.28

--- config/ember-try.js
+++ config/ember-try.js
@@ -10,18 +10,18 @@ module.exports = async function () {
       embroiderSafe(),
       embroiderOptimized(),
       {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
             'ember-source': '~3.28.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.4.0',
           },
         },
       },
