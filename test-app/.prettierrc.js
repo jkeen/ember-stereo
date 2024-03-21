15a6e92fdae282ed4c368e3f1659bc2217649d3d
Jeff Keen <jeff@keen.me>
2024-03-21 10:18:52 -0500

    update test app to 5.x

--- test-app/.prettierrc.js
+++ test-app/.prettierrc.js
@@ -1,7 +1,12 @@
 'use strict';
 
 module.exports = {
-  singleQuote: true,
-  tabWidth: 2,
-  useTabs: false,
+  overrides: [
+    {
+      files: '*.{js,ts}',
+      options: {
+        singleQuote: true,
+      },
+    },
+  ],
 };
