61ab6b0a47d21b66197be6ba9ac2a710a0a8bfb6
Jeff Keen <jeff@keen.me>
2024-10-13 09:34:22 -0500

    chore: Reconfigure watch and start tasks to reload when changes happen

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -4,7 +4,18 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    autoImport: {
+      watchDependencies: ['ember-stereo'],
+      webpack: {
+        devtool: 'source-map',
+      },
+    },
+    sourcemaps: {
+      enabled: true,
+    },
+    babel: {
+      plugins: [require.resolve('ember-auto-import/babel-plugin')],
+    },
   });
 
   return app.toTree();
