43613f38c14bf926615e6c4a59e4adefc0834f9e
Jeff Keen <jeff@keen.me>
2025-11-20 15:27:08 -0600

    chore: update docs app to ember 5.4

--- docs/.prettierrc.js
+++ docs/.prettierrc.js
@@ -4,4 +4,12 @@ module.exports = {
   singleQuote: true,
   tabWidth: 2,
   useTabs: false,
+  overrides: [
+    {
+      files: '*.{js,ts}',
+      options: {
+        singleQuote: true,
+      },
+    },
+  ],
 };
