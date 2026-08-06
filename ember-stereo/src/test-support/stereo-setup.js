a1d0f2295ae2aacf5ea0963c30906f30526f9759
Jeff Keen <jeff@keen.me>
2026-08-06 13:33:58 -0500

    fix: remove hard sinon dependency, turn it into an optional peer dependency and import it directly when using setupStereoTest
    fixes #50

--- ember-stereo/src/test-support/stereo-setup.js
+++ ember-stereo/src/test-support/stereo-setup.js
@@ -1,16 +1,9 @@
 import { next } from '@ember/runloop';
 import FakeMediaElement from './utils/fake-media-element';
 import { _cancelTimers as cancelTimers } from '@ember/runloop';
-
-// FIX ME: this will bite someone, but webpack is not picking up and exporting sinon properly
-// with the next line included in the build, the tests will fail with an error like:
-// TypeError: sinon__WEBPACK_IMPORTED_MODULE_2___default(...).createSandbox is not a function.
-// The tests pass without that for now, so I'm proceeding.
-
-// import sinon from 'sinon'
+import sinon from 'sinon';
 
 export function stubAudio() {
-  // eslint-disable-next-line no-undef
   let stereoSandbox = sinon.createSandbox({});
 
   let originalCreateElement = document.createElement;
@@ -56,7 +49,7 @@ export function setupStereoTest(hooks) {
 export function stubConnectionCreateWithSuccess(
   service,
   connectionName,
-  sandbox = sinon // eslint-disable-line no-undef
+  sandbox = sinon
 ) {
   let Connection = service.connectionLoader.get(connectionName);
   sandbox.stub(Connection, 'canPlay').returns(true);
@@ -70,7 +63,7 @@ export function stubConnectionCreateWithSuccess(
 export function stubConnectionCreateWithFailure(
   service,
   connectionName,
-  sandbox = sinon // eslint-disable-line no-undef
+  sandbox = sinon
 ) {
   let Connection = service.connectionLoader.get(connectionName);
   sandbox.stub(Connection, 'canPlay').returns(true);
